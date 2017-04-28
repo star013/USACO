296cc0e4fbc9b3787e6e801eebece2acae7584b1
star013 <thustar@sina.com>
2017-04-28 23:02:49 +0800

    4.28

---
+++ section3.1/agrinet.cpp
@@ -0,0 +1,80 @@
+﻿/*
+ID: thustar1
+PROG: agrinet
+LANG: C++11
+*/
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+
+int main() {
+	ofstream fout("agrinet.out");
+	ifstream fin("agrinet.in");
+	int n;
+	fin >> n;//n<=100
+	int g[100][100];
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			fin >> g[i][j];
+		}
+	}
+
+	/*for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << g[i][j] << " ";
+		}
+		cout << endl;
+	}*/
+
+	// Prim Algorithm
+	int INF = 100 * 100000;// because each weight<=100,000 and n<=100
+	bool in[100]; // in[i]==true if i belongs to the min span tree
+	int d[100]; // d[i] == distance between i and min span tree
+	int res = 0;
+	for (int i = 0; i < n; i++) {
+		in[i] = false;
+		d[i] = INF;
+	}
+	// select first node
+	int num = 1;
+	int idx = 0;
+	d[0] = 0;
+	in[0] = true;
+	while (num < n) {
+		// update distance
+		for (int i = 0; i < n; i++) {
+			if (!in[i]) {
+				if (d[i] > g[idx][i])
+					d[i] = g[idx][i];
+			}
+		}
+		// select nearest vertex
+		int tmp = INF;
+		for (int i = 0; i < n; i++) {
+			if (!in[i]) {
+				if (d[i] < tmp) {
+					tmp = d[i];
+					idx = i;
+				}
+			}
+		}
+		// add vertex
+		res += tmp;
+		in[idx] = true;
+		num++;
+		
+		// show test
+		/*cout << num << " nodes in tree" << endl;
+		for (int i = 0; i < n; i++) {
+			cout << "Node " << i << " d=" << d[i] << " in=" << in[i] << endl;
+		}
+		cout << "res=" << res << endl;*/
+	}
+	fout << res << endl;
+	return 0;
+}
---
+++ section3.1/inflate.cpp
@@ -0,0 +1,59 @@
+﻿/*
+ID: thustar1
+PROG: inflate
+LANG: C++11
+*/
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+struct Course
+{
+	int score;
+	int time;
+};
+
+int cmp(const void* a, const void* b) {
+	Course* c1 = (Course*)a;
+	Course* c2 = (Course*)b;
+	return c1->time - c2->time;
+}
+int main() {
+	ofstream fout("inflate.out");
+	ifstream fin("inflate.in");
+	int m, n;
+	fin >> m >> n;
+	Course c[10000];
+	for (int i = 0; i < n; i++) {
+		fin >> c[i].score >> c[i].time;
+	}
+	qsort(c, n, sizeof(Course), cmp);
+	/*for (int i = 0; i < n; i++) {
+		cout << "score=" << c[i].score << " time=" << c[i].time << endl;
+	}*/
+
+	int res[10001];
+	// dynamic programming, initialize
+	for (int i = 0; i <= m; i++)
+		res[i] = 0;
+	// F(T) = max{s[i]+F(T-t[i])}
+	for (int i = 0; i <= m; i++) {
+		// i==total time
+		for (int j = 0; j < n; j++) {
+			if (i >= c[j].time) {
+				if (res[i] < res[i - c[j].time] + c[j].score)
+					res[i] = res[i - c[j].time] + c[j].score;
+			}
+		}
+	}
+	// test
+	/*for (int i = 0; i <= m; i++) {
+		cout << "Total time=" << i << " Best score=" << res[i] << endl;
+	}*/
+	fout << res[m] << endl;
+	return 0;
+}
