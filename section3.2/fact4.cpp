01c7508871469f9b1e521154640dca461b2aa222
star013 <thustar@sina.com>
2017-05-14 19:51:13 +0800

    5.14

---
+++ section3.2/fact4.cpp
@@ -0,0 +1,103 @@
+﻿/*
+ID: thustar1
+PROG: fact4
+LANG: C++11
+*/
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+#define DIGIT 1100
+
+void multi(short* a, short end, short m, short* res, short pos) {
+	// end == start position of multiplication
+	// m == 0-9
+	// pos == start position of result
+	short carry = 0; // describe carry number
+	for (short i = end; i < DIGIT-pos; i++) {
+		res[i+pos] = a[i] * m + carry;
+		carry = res[i + pos] / 10;
+		res[i + pos] = res[i + pos] % 10;
+	}
+}
+
+void add(short* a , short* b, short end, short* res) {
+	short carry = 0;
+	for (short i = end; i < DIGIT; i++) {
+		res[i] = a[i] + b[i] + carry;
+		carry = res[i] / 10;
+		res[i] = res[i] % 10;
+	}
+}
+
+void multiply(short* a, short end, short m, short* res) {
+	// end == start position of multiplication
+	// m == a number with arbitrary digit
+	short mres[DIGIT] = { 0 };
+	short i = 0;
+	while (m > 0) {
+		multi(a, end, m % 10, mres, i);
+		add(res, mres, end, res);
+		mres[end+i] = 0; // recover
+		i++;
+		m /= 10;
+	}
+}
+int main() {
+	ofstream fout("fact4.out");
+	ifstream fin("fact4.in");
+	short n;//n<=4220
+	fin >> n;
+	// 4220! has 4220/5+4220/25+4220/125+4220/625+4220/3125=1054 < 1100 zeros
+	// It is why choose DIGIT as 1100
+	short a[DIGIT] = { 0 };
+	a[0] = 1;
+	short end = 0;
+	short tmp[DIGIT] = { 0 };
+	for (short i = 2; i <= n; i++) {
+		multiply(a, end, i, tmp);
+		if (i % 5 == 0) {
+			while (tmp[end] == 0)
+				end++;
+		}
+		for (short j = 0; j < DIGIT; j++) {
+			a[j] = tmp[j];
+			tmp[j] = 0; // recover tmp
+		}
+		/*cout << "i=" << i << endl;
+		for (short j = 0; j < 10; j++)
+			cout << a[9 - j];
+		cout << endl << endl;*/
+	}
+	fout << a[end] << endl;
+	/*short a[DIGIT] = { 9,9,2,0,0 };
+	short b[DIGIT];
+	multi(a, 0, 7, b, 1);
+	for (short i = 0; i < DIGIT; i++) {
+		cout << b[DIGIT - 1 - i]<<" ";
+	}
+	cout << endl;
+	multi(a, 0, 8, b, 0);
+	for (short i = 0; i < DIGIT; i++) {
+		cout << b[DIGIT - 1 - i] << " ";
+	}
+	cout << endl;
+	short c[DIGIT] = { 9,8,7,6,0 };
+	short d[DIGIT];
+	add(a, c, 0, d);
+	for (short i = 0; i < DIGIT; i++) {
+		cout << d[DIGIT - 1 - i] << " ";
+	}
+	cout << endl;
+	short e[DIGIT] = { 0 };
+	multiply(a, 0, 678, e);
+	for (short i = 0; i < DIGIT; i++) {
+		cout << e[DIGIT - 1 - i] << " ";
+	}
+	cout << endl;*/
+	return 0;
+}
---
+++ section3.2/kimbits.cpp
@@ -0,0 +1,101 @@
+﻿/*
+ID: thustar1
+PROG: kimbits
+LANG: C++11
+*/
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int combine(int n, int k) {
+	// C(n,k) = n!/k!/(n-k)!
+	if (k > n / 2)
+		k = n - k;
+	double tn = (double)n;
+	double tk = (double)k;
+	double res = 1;
+	for (int i = 0; i < k; i++)
+		res = res*(tn - i) / (i + 1);
+	/*for (int i = k; i > 1; i--)
+		res = res / i;*/
+	return (int)res;
+}
+
+int total(int n, int k) {
+	// return C(n,0)+C(n,1)+...+C(n,k-1)+C(n,k)
+	if (n <= k)
+		return (int)pow(2, n);
+	// n>k
+	int res = 0;
+	for (int i = 0; i <= k; i++) {
+		res += combine(n, i);
+	}
+	return res;
+}
+
+int main() {
+	ofstream fout("kimbits.out");
+	ifstream fin("kimbits.in");
+	unsigned int n, L, p;//l<=n<=31
+	// Test 10:31 31 2147483648 
+	// TLE
+	// because int maximum is 2147483647, it can not read 2147483648
+	// it will be an error!
+	fin >> n >> L >> p;
+	//cout << n << " " << L << " " << p << endl;
+	//cout << combine(20, 4) << endl;
+	//cout << combine(20, 0) << endl;
+	//cout << total(0, 3) << endl;
+	//cout << total(1, 3) << endl;
+	//cout << total(2, 3) << endl;
+
+	bool one[32];
+	for (int i = 0; i < 32; i++) {
+		one[i] = false;
+	}
+	int pos = n;//tl=l
+	while (pos >= 0) {
+		int right = 1;// if no "1", all bits are "0", it is one combination
+		if (right == p) {
+			pos = -1;
+			p -= right; 
+			break;
+		}
+		// right<p
+		for (int i = 0; i < pos; i++) {
+			int tmp = total(i, L - 1);
+			if (right+tmp > p) {
+				// total(i,L-1)==total combination when the first "1" is at the position i
+				// if there are L "1"s, then there are at most L-1 "1"s at the right side of position i
+				pos = i;
+				break;
+			}
+			else if (right + tmp == p) {
+				// select maximum possible value
+				for (int j = 0; j < L; j++) {
+					if (i - j >= 0)
+						one[i - j] = 1;
+				}
+				pos = -1;
+				break;
+			}
+			else
+				right += tmp;
+		}
+		if (pos >= 0) {
+			one[pos] = true;
+			p -= right;
+			L--;
+		}
+	}
+	
+	for (int i = 0; i < n; i++) {
+		fout << (int)one[n - 1 - i];
+	}
+	fout << endl;
+	return 0;
+}
